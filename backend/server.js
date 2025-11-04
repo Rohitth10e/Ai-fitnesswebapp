578d9d15e43b29a9e54616e3eeca92d2c1f5c08a
rohith10e <rohith.e.s82@gmail.com>
2025-11-04 15:10:31 +0530

    fix: cors

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://ai-fitnesswebapp.netlify.app' 
+];
+
 const connectDB = async() => {
     try {
         await mongoose.connect(MONGO_URI);
@@ -22,7 +27,15 @@ const connectDB = async() => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('CORS policy does not allow access from this origin.'));
+        }
+    }
+}));
 app.use('/apiv1/users', routes);
 
 app.listen(PORT, () => {
